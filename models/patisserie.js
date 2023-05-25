0fbd00c10ebaa1566b2f622ca455c47d570728a7
Marty <martyrabord@yahoo.fr>
2023-05-25 16:28:53 +0200

    maj user and patisserie

--- models/patisserie.js
+++ models/patisserie.js
@@ -1,20 +1,69 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+require('dotenv').config();
+
 
 const patisserieSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true
+    required: true,
   },
-  number: {
+  stock: {
     type: Number,
-    required: true
+    required: true,
   },
-  order: {
-    type: Number,
-    required: true
-  }
 });
 
-const Patisserie = mongoose.model('Patisserie', patisserieSchema);
+const Patisserie = mongoose.model("Patisserie", patisserieSchema);
 
+// Connexion à la base de données MongoDB
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Supprimer une pâtisserie du stock
+const removePatisserieFromStock = async (patisserieName) => {
+  try {
+    const patisserie = await Patisserie.findOne({ name: patisserieName });
+    if (patisserie) {
+      patisserie.stock--;
+      await patisserie.save();
+      console.log(`Patisserie "${patisserieName}" enlevée du stock.`);
+    } else {
+      console.log(`Patisserie "${patisserieName}" non trouvée.`);
+    }
+  } catch (error) {
+    console.error(
+      "Une erreur s'est produite lors de la suppression de la pâtisserie du stock :",
+      error.message
+    );
+  }
+};
+
+// Création des documents de pâtisseries avec le stock initial
+const createPatisserie = async (name, stock) => {
+  try {
+    const patisserie = new Patisserie({ name, stock });
+    await patisserie.save();
+    console.log(
+      `Patisserie "${name}" créée avec un stock initial de ${stock}.`
+    );
+  } catch (error) {
+    console.error(
+      "Une erreur s'est produite lors de la création de la pâtisserie :",
+      error.message
+    );
+  }
+};
+
+// Création des pâtisseries avec les stocks initiaux
+createPatisserie("Brioche sucrée avec chocolat", 15);
+createPatisserie("Cake glacé fondant au chocolat", 35);
+createPatisserie("Tarte aux fruits", 24);
+createPatisserie("Éclair au café", 30);
+createPatisserie("Croissant aux amandes", 25);
+
+// Exemple d'utilisation pour enlever une pâtisserie du stock
+const patisserieName = "Brioche sucrée avec chocolat";
+removePatisserieFromStock(patisserieName);
 module.exports = Patisserie;
--- models/user.js
+++ models/user.js
@@ -1,10 +1,17 @@
 const mongoose = require("mongoose")
+const Patisserie = require("./patisserie")
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true },
   password: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
   pastriesWon: [
+    {
+      name: { type: String, required: true },
+      quantity: { type: Number, required: true, default: 0 },
+    },
+  ],
+  pastriesStock: [
     {
       name: { type: String, required: true },
       quantity: { type: Number, required: true, default: 0 },
@@ -22,6 +29,23 @@ userSchema.methods.addWonPastry = async function (patisserieName) {
   } else {
     this.pastriesWon.push({ name: patisserieName, quantity: 1 })
   }
+
+  // Ajouter la pâtisserie gagnée au stock de l'utilisateur
+  const userPastryIndex = this.pastriesStock.findIndex(
+    (pastry) => pastry.name === patisserieName
+  )
+
+  if (userPastryIndex !== -1) {
+    this.pastriesStock[userPastryIndex].quantity++
+  } else {
+    this.pastriesStock.push({ name: patisserieName, quantity: 1 })
+  }
+
+  // Supprimer la pâtisserie gagnée de la collection de pâtisseries
+  await Patisserie.findOneAndUpdate(
+    { name: patisserieName },
+    { $inc: { number: -1 } }
+  )
 
   await this.save()
 }
